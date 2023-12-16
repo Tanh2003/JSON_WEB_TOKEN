e30657171012aee6b1f693a1bd094fb3a941f5a6
Tanh2003 <nhoxtuananh092@gmail.com>
2023-12-16 22:03:29 +0700

    video 3

--- JWT_BE/src/routes/web.js
+++ JWT_BE/src/routes/web.js
@@ -1,19 +1,20 @@
 import express from "express";
+import homeController from "../controller/homeController";
 
-const router =express.Router();
+const router = express.Router();
 
 /**
- * 
+ *
  * @param {*} app :express app
  */
 
-const initWebRoutes=(app)=>{
-    router.get("/",(req,res)=>{
-        return res.send("hello world");
-    })
+const initWebRoutes = (app) => {
+  router.get("/", homeController.handleHelloword);
 
-    return app.use("/",router);
+  router.get("/userpage",homeController.handleUserPage);
 
-}
+  // return app.use("/abc",router);  example stress link  localhost:8080/ab/aboutme mới được
+  return app.use("/", router);
+};
 
 export default initWebRoutes;
